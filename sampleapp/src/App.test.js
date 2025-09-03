4a93e00807bb5454c06797bbbeb1ed15f74943a6
vinay-hegde17 <vinay.hegde@phyelements.com>
2025-09-03 16:02:28 +0530

    testcase updated

--- sampleapp/src/App.test.js
+++ sampleapp/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the logo image', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logoElement = screen.getByAltText(/logo/i);
+  expect(logoElement).toBeInTheDocument();
 });
