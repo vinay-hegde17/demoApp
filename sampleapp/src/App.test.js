ece12b3d7668dd611bfb8dfd3a07a468d309265a
vinay-hegde17 <vinay.hegde@phyelements.com>
2025-09-04 18:23:16 +0530

    test suites added

--- sampleapp/src/App.test.js
+++ sampleapp/src/App.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the logo image', () => {
-  render(<App />);
-  const logoElement = screen.getByAltText(/logo/i);
-  expect(logoElement).toBeInTheDocument();
-});
+describe('App component', () => {
+  test('renders the logo image', () => {
+    render(<App />);
+    const logoElement = screen.getByAltText(/logo/i);
+    expect(logoElement).toBeInTheDocument();
+  });
 
-test('renders the text', () => {
-  render(<App />);
-const textElement = screen.getByText(/learn react/i);
-expect(textElement).toBeInTheDocument();
+  test('renders the text', () => {
+    render(<App />);
+    const textElement = screen.getByText(/learn react/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });
