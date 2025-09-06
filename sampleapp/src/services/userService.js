b1fcf48c0db4d3d603938b93ce0af50a7c3a4cce
vinay-hegde17 <vinay.hegde@phyelements.com>
2025-09-06 15:15:06 +0530

    port issue resolved

--- sampleapp/src/services/userService.js
+++ sampleapp/src/services/userService.js
@@ -3,5 +3,5 @@ const API_URL = "http://localhost:5000";
 export const fetchUsers = async () => {
   const res = await fetch(`${API_URL}/users`);
   if (!res.ok) throw new Error("Failed to fetch users");
-  return res.json();
+  return await res.json();
 };
