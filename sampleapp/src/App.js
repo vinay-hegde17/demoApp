04c19c5a7f760d92a7850d7d820d95a25c892591
vinay-hegde17 <vinay.hegde@phyelements.com>
2025-09-08 17:37:43 +0530

    test case related contents added again

--- sampleapp/src/App.js
+++ sampleapp/src/App.js
@@ -1,10 +1,18 @@
+import logo from './logo.svg';
 import './App.css';
 import UserData from './component/UserData';
 
 function App() {
   return (
     <div className="App">
-    <UserData />
+      <UserData />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit src/App.js and save to reload to learn react.
+          Updated the App.js file to test the feature which is added.
+        </p>
+      </header>
     </div>
   );
 }
