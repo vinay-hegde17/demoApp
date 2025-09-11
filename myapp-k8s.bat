757b6d4ac512a9b781d2bea3dfa3dc73d7c05c64
vinay-hegde17 <vinay.hegde@phyelements.com>
2025-09-11 17:57:40 +0530

    Always pull the latest images we pod is update

--- myapp-k8s.bat
+++ myapp-k8s.bat
@@ -5,8 +5,5 @@ minikube start
 echo Applying Kubernetes manifests...
 kubectl apply -f k8s/
 
-echo Cleaning up unused images inside Minikube...
-minikube ssh "docker image prune -af"
-
 echo Forwarding frontend-service to http://127.0.0.1:4040
 kubectl port-forward svc/frontend-service 4040:80
