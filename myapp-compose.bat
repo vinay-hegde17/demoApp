a7971f18dfcfb63ff808ecd91d6ab02f11095f55
vinay-hegde17 <vinay.hegde@phyelements.com>
2025-09-11 13:38:24 +0530

    Prune the intermediate images once after fetch from GHCR

--- myapp-compose.bat
+++ myapp-compose.bat
@@ -1,13 +1,13 @@
 @echo off
-echo Cleaning up dangling images...
-docker image prune -af
-
 echo Pulling latest images...
 docker pull ghcr.io/vinay-hegde17/demoapp-frontend:latest
 docker pull ghcr.io/vinay-hegde17/demoapp-backend:latest
 
 echo Starting containers with Docker Compose...
 docker-compose up -d --force-recreate
+
+echo Cleaning up dangling images...
+docker image prune -af
 
 echo App is running at:
 echo   Frontend -^> http://localhost:3030
