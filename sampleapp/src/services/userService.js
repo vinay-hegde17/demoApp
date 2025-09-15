76bb5dcb8df8d55a35caf26d052a71f87713049c
vinay-hegde17 <vinay.hegde@phyelements.com>
2025-09-15 14:00:09 +0530

    Issue fixing one of debugging data from node app to react

--- sampleapp/src/services/userService.js
+++ sampleapp/src/services/userService.js
@@ -1,7 +1,19 @@
 const API_URL = process.env.REACT_APP_API_URL;
+console.log("API_URL:", API_URL);
 
 export const fetchUsers = async () => {
-  const res = await fetch(`${API_URL}/users`);
-  if (!res.ok) throw new Error("Failed to fetch users");
-  return await res.json();
+  try {
+    const res = await fetch(`${API_URL}/users`);
+    console.log("Response:", res);
+
+    if (!res.ok) {
+      throw new Error(`Server responded with status ${res.status}`);
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching users:", error.message);
+    throw error;
+  }
 };
